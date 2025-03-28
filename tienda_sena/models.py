cb103d1b73363e52a4ed6accf73047c823be0c2b
DarkJ-18 <j.juancamilojurado@gmail.com>
2025-03-27 22:39:13 -0500

    v1.1.0-alpha.1

--- tienda_sena/models.py
+++ tienda_sena/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    nombre_apellido = models.CharField(max_length=150)
+    correo = models.CharField(max_length=254)
+    password = models.CharField(max_length=254)
+    ROLES = (
+        (1, "Administrador"),
+        (2, "Cliente"),
+        (3, "Vendedor"),
+    )
+    rol = models.IntegerField(choices=ROLES, default=2)
+#    imagen_perfil = models.ImageField(upload_to='usuarios/', null=True, blank=True)  # Campo para la foto de perfil
+
+    def __str__(self):
+        return f"{self.nombre} - {self.rol}"
