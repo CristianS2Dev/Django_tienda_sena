fefc9dc1b8a88ab359bc5a3f3d7ef50f67278707
DarkJ-18 <j.juancamilojurado@gmail.com>
2025-03-29 15:22:48 -0500

    v1.7.3-alpha.1

--- tienda_sena/urls.py
+++ tienda_sena/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     path('editar_producto/<int:id_producto>/', views.editar_producto, name='editar_producto'),
     path('producto/<int:id_producto>/', views.detalle_producto, name='detalle_producto'),
     path('eliminar_producto/<int:id_producto>/', views.eliminar_producto, name='eliminar_producto'),
+     path('productos/categoria/<int:categoria>/', views.productos_por_categoria, name='productos_por_categoria'),
     
     #Crud Usuarios
     path('usuarios', views.usuarios, name='usuarios'),
